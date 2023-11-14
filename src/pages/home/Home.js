c82fcbc60fdede0d72fbcff71a00cbae73b662c1
Harim Kim <rimiha9708@gmail.com>
2023-11-14 17:32:45 +0900

    main banner

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { nowPlaying } from "../../api";
+import { IMG_URL } from "../../constants";
 
 const MainBanner = styled.section`
   height: 80vh;
   background-color: lightgray;
   position: relative;
   padding: 400px 5%;
+  background: url(${IMG_URL}/original/${(props) => props.$bgUrl}) no-repeat
+    center / cover;
   h3,
   p {
     position: relative;
   }
 
   h3 {
+    max-width: 650px;
+    width: 100%;
     font-size: 80px;
     font-weight: 700;
     margin-bottom: 30px;
@@ -21,10 +26,23 @@ const MainBanner = styled.section`
   }
 
   p {
+    max-width: 650px;
+    width: 100%;
     font-size: 18px;
     font-weight: 400;
     line-height: 26px;
     opacity: 0.8;
+  }
+
+  @media screen and (max-width: 450px) {
+    h3 {
+      font-size: 50px;
+      line-height: 65px;
+    }
+
+    p {
+      font-size: 16px;
+    }
   }
 `;
 
@@ -51,7 +69,7 @@ export const Home = () => {
   const [nowPlayingData, setNowPlayingData] = useState();
   // =>밖에서도 지역변수였던 results를 사용하기 위해서 useState 사용(함수 밖으로 빼내기 위함)
 
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
@@ -61,32 +79,27 @@ export const Home = () => {
         // => results는 비구조화할당으로 배열이기 때문에 위처럼 [n번째]를 사용해야함
         setNowPlayingData(results);
         // =>밖에서도 지역변수였던 results를 사용하기 위해서
-        setLoading(false);
+        setIsLoading(false);
       } catch (error) {
         console.log("에러: " + error);
       }
     })();
   }, []);
 
-  console.log(loading);
+  console.log(isLoading);
   console.log(nowPlayingData);
 
   return (
     <>
-      {loading ? (
+      {isLoading ? (
         "loading..."
       ) : (
         <div>
           {nowPlayingData && (
-            <MainBanner>
+            <MainBanner $bgUrl={nowPlayingData[0].backdrop_path}>
               <BlackBg />
               <h3>{nowPlayingData[0].title}</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-                sit eligendi ipsa officia a facilis eos nostrum, voluptas
-                ratione repudiandae neque nemo at earum saepe omnis optio vero
-                vitae consequuntur!
-              </p>
+              <p>{nowPlayingData[0].overview.slice(0, 100) + "..."}</p>
             </MainBanner>
           )}
         </div>
